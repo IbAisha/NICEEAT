05abab48550785424bfae5609659887aea7d63ac
Aïsha <zawjatu.a@gmail.com>
2023-01-20 11:44:14 +0100

    pages de base
    Index + navbar + sidebar + dossier images

---
+++ parts/navbar.php
@@ -0,0 +1,57 @@
+<body>
+<nav>
+    <!--hamburger menu-->
+
+    <a href="#" id="openBtn"> <!--bouton d'ouverture -->
+      <div class="burger-icon">
+        <div class="burger-line"></div>
+        <div class="burger-line"></div>
+        <div class="burger-line"></div>
+      </div>
+    </a>
+    
+
+    <!--Laptop-->
+
+    <div class="list-navbar">
+      <ul>
+          <li><a href="./index.php">Accueil</a></li>
+          <li><a href="./menus.php">Nos menus</a></li>
+          <li><a href="./snacking.php">Nos snacks</a></li>
+          <li><a href="./snacking.php">Click & collect</a></li>
+        </ul>
+    </div>
+
+
+    <!--LOGO AU CENTRE DE LA NAVBAR-->
+    <a href="./index.php" id="logo-niceeat"><img src="./img/logoBlack.png" alt="logo NiceEat" width ="100px"></a>
+    
+
+    <!--ICON LOGIN-->
+
+    <!--TEL/TABLET-->
+
+    <div class="icon">
+        <a href="form.login.php" id="logo-icon">
+        <img src="./img/login-icon.png"  alt="logo de connexion" width="40px">
+      </a>
+    </div>
+
+
+<?php require_once "sidenav.php"; ?>
+
+
+    <!--DESKTOP-->
+
+    <div class="buttons">
+      <button id="login"><a href="./form.login.php">Se connecter</a></button>
+      <button id="signup"><a href="./form.signup.php">S'inscrire</a></button>
+    </div>
+
+
+    </nav>
+  
+  <!--END NAVBAR SECTION-->
+
+<script src="main.js"></script>
+</body>
---
+++ parts/sidenav.php
@@ -0,0 +1,24 @@
+<!--SIDE BAR : Out of nav section because it applies an unwanted nav properties-->
+<div id="mySidenav" class="sidenav">
+      <a id="closeBtn" href="#" class="close">
+      <div id="top-cross"></div>
+      <div id="bottom-cross"></div></a>
+
+      <ul>
+        <li><a href="./index.php">Acceuil</a></li>
+        <li><a href="./menus.php">Nos menus</a></li>
+        <li><a href="./snacking.php">Nos snacks</a></li>
+        <li><a href="./blog.php">Blog</a></li>
+        <li><a href="#">Nos avis</a></li>
+        <li><a href="#" id="clickAndCo">CLICK & COLLECT</a></li>
+      </ul>
+
+      <div class="logo-insta">
+      <a href="http://instagram.com/byniceeat" id="insta-logo"><img src="./img/insta-logo.png"  width="40px" alt="logo instagram"></a>
+      <a href="http://instagram.com/byniceeat" id="insta-tiktok"><img src="./img/insta-tiktok.png"  width="40px" alt="logo tiktok"></a>
+      </div>
+    
+      
+</div>
+
+<script src="main.js"></script>
